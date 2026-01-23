4147516b224f1a5189e5e50859603622979c58ed
Sam Lambrick <34570926+slambrick@users.noreply.github.com>
2026-01-23 08:42:35 +0000

    Update constants.py
    Conversion functions for multiple species

--- constants.py
+++ constants.py
@@ -21,138 +21,200 @@ mHe3 = 3.0160293*mu # Mass of He-3 [kg]
 meV2J = 1.6021892*1E-22  # Convert from meV to J
 
 # Neutron conversion functions
-def n_lambda_to_E(wavelength):
+# Species identification and mass lookup
+
+def get_species_mass(species):
     """
-    Convert neutron wavelength to energy
+    Get the mass of a particle species from a string identifier
+    
+    Parameters
+    ----------
+    species : str
+        Species identifier (e.g., 'n', 'neutron', 'He-3', 'he4', etc.)
+    
+    Returns
+    -------
+    mass : float
+        Particle mass [kg]
+    """
+    species_lower = species.lower().replace(' ', '').replace('-', '')
+    
+    # Neutron
+    if species_lower in ['n', 'neutron']:
+        return mn
+    
+    # Helium-3
+    elif species_lower in ['he3', 'helium3', '3he']:
+        return mHe3
+    
+    # Helium-4
+    elif species_lower in ['he4', 'helium4', '4he']:
+        return mHe
+    
+    # Generic helium (defaults to He-4)
+    elif species_lower in ['he', 'helium']:
+        print("Warning: 'He' specified without isotope, defaulting to He-4")
+        return mHe
+    
+    else:
+        raise ValueError(f"Unknown species: {species}. Use 'n', 'He-3', 'He-4', or 'He'")
+
+
+# Conversion functions
+def lambda_to_E(wavelength, species='n'):
+    """
+    Convert wavelength to energy
     
     Parameters
     ----------
     wavelength : float or array
-        Neutron wavelength [m]
+        Wavelength [m]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
     """
-    energy = (h**2 / (2 * mn * wavelength**2)) / meV2J
+    m = get_species_mass(species)
+    energy = (h**2 / (2 * m * wavelength**2)) / meV2J
     return energy
 
-def n_E_to_lambda(energy):
+def E_to_lambda(energy, species='n'):
     """
-    Convert neutron energy to wavelength
+    Convert energy to wavelength
     
     Parameters
     ----------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     wavelength : float or array
-        Neutron wavelength [m]
+        Wavelength [m]
     """
-    wavelength = h / np.sqrt(2 * mn * energy * meV2J)
+    m = get_species_mass(species)
+    wavelength = h / np.sqrt(2 * m * energy * meV2J)
     return wavelength
 
-def n_lambda_to_p(wavelength):
+def lambda_to_p(wavelength, species='n'):
     """
-    Convert neutron wavelength to momentum
+    Convert wavelength to momentum
     
     Parameters
     ----------
     wavelength : float or array
-        Neutron wavelength [m]
+        Wavelength [m]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     momentum : float or array
-        Neutron momentum [kg⋅m/s]
+        Momentum [kg⋅m/s]
     """
     momentum = h / wavelength
     return momentum
 
-def n_p_to_lambda(momentum):
+def p_to_lambda(momentum, species='n'):
     """
-    Convert neutron momentum to wavelength
+    Convert momentum to wavelength
     
     Parameters
     ----------
     momentum : float or array
-        Neutron momentum [kg⋅m/s]
+        Momentum [kg⋅m/s]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     wavelength : float or array
-        Neutron wavelength [m]
+        Wavelength [m]
     """
     wavelength = h / momentum
     return wavelength
 
-def n_E_to_p(energy):
+def E_to_p(energy, species='n'):
     """
-    Convert neutron energy to momentum
+    Convert energy to momentum
     
     Parameters
     ----------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     momentum : float or array
-        Neutron momentum [kg⋅m/s]
+        Momentum [kg⋅m/s]
     """
-    momentum = np.sqrt(2 * mn * energy * meV2J)
+    m = get_species_mass(species)
+    momentum = np.sqrt(2 * m * energy * meV2J)
     return momentum
 
-def n_p_to_E(momentum):
+def p_to_E(momentum, species='n'):
     """
-    Convert neutron momentum to energy
+    Convert momentum to energy
     
     Parameters
     ----------
     momentum : float or array
-        Neutron momentum [kg⋅m/s]
+        Momentum [kg⋅m/s]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
     """
-    energy = (momentum**2 / (2 * mn)) / meV2J
+    m = get_species_mass(species)
+    energy = (momentum**2 / (2 * m)) / meV2J
     return energy
 
-def n_v_to_E(velocity):
+def v_to_E(velocity, species='n'):
     """
-    Convert neutron velocity to energy
+    Convert velocity to energy
     
     Parameters
     ----------
     velocity : float or array
-        Neutron velocity [m/s]
+        Velocity [m/s]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
     """
-    energy = (0.5 * mn * velocity**2) / meV2J
+    m = get_species_mass(species)
+    energy = (0.5 * m * velocity**2) / meV2J
     return energy
 
-def n_E_to_v(energy):
+def E_to_v(energy, species='n'):
     """
-    Convert neutron energy to velocity
+    Convert energy to velocity
     
     Parameters
     ----------
     energy : float or array
-        Neutron energy [meV]
+        Energy [meV]
+    species : str, optional
+        Particle species (default: 'n')
     
     Returns
     -------
     velocity : float or array
-        Neutron velocity [m/s]
+        Velocity [m/s]
     """
-    velocity = np.sqrt(2 * energy * meV2J / mn)
+    m = get_species_mass(species)
+    velocity = np.sqrt(2 * energy * meV2J / m)
     return velocity
